c8b35e4a8759221dd0d5399e26710bca10f2a390
AnsloneKritisch <digveshoptimus@gmail.com>
2021-07-26 00:56:16 +0530

    strating

---
+++ Operators_in_python.py
@@ -0,0 +1,50 @@
+"""
+Operation in  python
+Arithmetic Operators
+Assigment operators
+comparison Opretors
+logical oprators
+identical operator
+memorary operator
+
+
+
+
+#arithmatic operator
++ addition operator
+- substraction operator
+* multiplication operator
+/ division operator
+// it gives quitent of a division
+% it gives remender of a division
+** it gives power to a number "
+
+
+
+#assigment operator
+x= 5
+print(x)
+x = x+7
+print(x)
+
+#comparison operator
+i=8
+print (i = =5 )
+
+#logical operators
+ a =true
+b =false
+print (a and b )
+
+#identity operators
+print(" 5 is not 5 ")
+
+#Membership operator
+list = [ 3,2,58,5,4,4,54,45,45,45,54,54,454,545,8]
+print(32 in list)
+
+#Bitwise operators
+print(0 and 1)
+print(0 or 1)
+
+"""
