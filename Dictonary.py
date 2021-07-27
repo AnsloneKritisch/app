7b49581e732fe86ed4deb0b51d0898b48198e6b7
AnsloneKritisch <digveshoptimus@gmail.com>
2021-07-28 00:57:39 +0530

    change

---
+++ Dictonary.py
@@ -0,0 +1,14 @@
+"""d1 = {}
+print(type(d1))
+d2 = {"Rohan" : "fish" ,"Raj" : "Cold Drink"}
+print(d2["Raj"])
+print(d2)
+d2["Rajan"] =  "Paneer"
+d3 =  d2.copy()
+del d2["Rajan"]
+print(d2)
+print(d3)
+d2.update({"Raj" : "Coffee"})
+print(d2.keys())
+print(d2.items())"""
+
