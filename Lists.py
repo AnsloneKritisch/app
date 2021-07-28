cffbc2483fe5d0b71cf921ac3f5b000e1c0b8394
AnsloneKritisch <digveshoptimus@gmail.com>
2021-07-29 00:14:50 +0530

    list

---
+++ Lists.py
@@ -0,0 +1,19 @@
+#List
+x = [ 1 , 2, 3,4,5]
+print(x)
+x.reverse()
+print(x)
+y = [5,6,5,98,8,6,7,5,9,5,8,5]
+y.sort()
+print (y)
+y.append(x)
+print(y)
+y.pop(5)#Here only index number is used
+print(y)
+#swap two number
+a=10
+b=20
+print(a,b)
+a,b = b,a
+print(a,b)
+
