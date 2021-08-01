2c9f0926c216a067d8f419b9f2c67af011819461
AnsloneKritisch <digveshoptimus@gmail.com>
2021-08-01 21:05:18 +0530

    one

---
+++ If,else and elif Statment.py
@@ -0,0 +1,9 @@
+"""a = 2
+b = 3
+c = int(input("Enter A number"))
+if a == c :
+    print(" c = 2 ")
+elif b == c :
+    print(" c = 3 ")
+else :
+    print("c is more than 3")"""
---
+++ for loop.py
@@ -0,0 +1,4 @@
+"""l = [ "A" , "B" , "C "  ]
+for i in l :
+    print(i)
+"""
