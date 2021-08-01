59347a1a08a0bdd51bb9f0f72e29e4745f6bc0e7
AnsloneKritisch <digveshoptimus@gmail.com>
2021-08-02 00:07:40 +0530

    try

---
+++ try and except .py
@@ -0,0 +1,11 @@
+"""n1 = int(input("write a number "))
+n2 = int(input("write a number "))
+try :
+    print(n1 + n2)
+except :
+    print (e)
+
+print ("this line is very important ")
+
+# did not understand the use of try and except function
+trying to understand it """
